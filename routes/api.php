126861cc600fb516f2ae4093596b7f471413375b
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2020-09-28 19:57:09 +0200

    Video Type

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VideoCatagoryController;
+use App\Http\Controllers\VideoTypeController;
+
 use App\Http\Controllers\VideoController;
 
 use App\Http\Controllers\Controller;
@@ -15,9 +17,9 @@ Route::post('/v1/auth', [UserController::class, 'auth']);
 
 Route::apiResource('/v1/user', UserController::class)->middleware('auth:api');
 
-Route::apiResource('/v1/videoCatagory', VideoCatagoryController::class);
-
 Route::apiResource('/v1/video', VideoController::class);
+Route::apiResource('/v1/videoCatagory', VideoCatagoryController::class);
+Route::apiResource('/v1/videotype', VideoTypeController::class);
 
 
 // Route::resource('/v1/user', UserController::class, ['except' => ['']]);
