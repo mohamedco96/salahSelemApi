debd9b70d39a6a20c58c85b25d0b520131b33144
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2020-10-28 18:27:17 +0200

    update package

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInite7f3f61d25e4a52ab4600df2bfd45eef
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInite7f3f61d25e4a52ab4600df2bfd45eef', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInite7f3f61d25e4a52ab4600df2bfd45eef', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInite7f3f61d25e4a52ab4600df2bfd45eef::getInitializer($loader));
         } else {
